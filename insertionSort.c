0b0f9b49e9770fe65b8b06d86a4e03c0f221e19f
Om Shahane <71377782+shahane806@users.noreply.github.com>
2021-12-04 20:44:50 +0530

    Add files via upload

---
+++ insertionSort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter array size\n");
+    scanf("%d",&n);
+    int arr[n],temp;
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for ( i = 1; i < n; i++)
+    {
+        temp = arr[i];
+        j=i-1;
+        while (j>=0&&arr[j]>temp)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+        
+    }
+    
+    for ( j = 0; j < n; j++)
+    {
+        printf("%d ",arr[j]);
+    }
+    printf("\n");
+    
+    
+
+    return 0;
+}
