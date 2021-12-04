ad802f88df2b455f68d50d5c6328fa4d375ade54
Om Shahane <71377782+shahane806@users.noreply.github.com>
2021-12-04 20:50:13 +0530

    Add files via upload

---
+++ linear_search.c
@@ -0,0 +1,28 @@
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int arr[10] = {4,3,2,6,1,5,7,9,8,0};
+    int n= 10,data,i;
+    printf("Enter data value\n");
+    scanf("%d",&data);
+    for ( i = 0; i < n; i++)
+    {
+        if (arr[i]==data)
+        {
+            printf("%d is Found\n",arr[i]);
+            break;
+        }
+        if (arr[i]==n)
+        {
+            printf("Data is not present\n");
+        }
+        
+        
+    }
+    
+    return 0;
+}
